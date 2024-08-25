1a6b8d38752211aa17d25078ce1a8387612f098c
JeongChan2 <jcstar55@gmail.com>
2024-08-25 19:54:29 +0900

    feat: counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import Box from './component/Box';
+import GrandSonBOx from './component/GrandSonBOx';
+import { Container, Row } from 'react-bootstrap';
+
 
 function App() {
+  const count = useSelector(state => state.count);
+  const id = useSelector(state => state.id);
+  const password = useSelector(state => state.password);
+
+  const dispatch = useDispatch();
+
+
+  const increase = () => {
+    dispatch({type:"INCREMENT", payload: {num:10}})
+    
+  }
+
+  const decrease = () => {
+    dispatch({type:"DECREASE", payload: {num:2}})
+  }
+
+  const login = () => {
+    dispatch({type:"LOGIN", payload:{id:"noo",password:"123"}})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Container>
+      <Row>
+        <div>
+          <h1>{id} {password}</h1>
+          <h1>{count}</h1>
+          <button onClick={increase}>증가</button>
+          <button onClick={decrease}>감소</button>
+          <Box />
+          <GrandSonBOx/>
+          <button onClick={login}>login</button>
+      </div>
+      </Row>
+    </Container>
+
   );
 }
 
